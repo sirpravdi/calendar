c3c79a7fae1ae354111b9a963ac0181250f959ea
sirpravdi <ollias17@gmail.com>
2019-02-10 20:42:27 +0300

    service worker test

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -9,7 +9,7 @@ var preLoad = function(){
   console.log('[PWA Builder] Install Event processing');
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Cached index and offline page during Install');
-    return cache.addAll(['/offline.html', '/calendar', '/calendar/calendar.js', '/calendar/calendar.css']);
+    return cache.addAll(['/calendar', '/calendar/calendar.js', '/calendar/calendar.css']);
   });
 }
 
