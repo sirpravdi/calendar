753e411f72338c9edcd8c2db8a97f8bef2007dc8
sirpravdi <ollias17@gmail.com>
2019-02-10 17:52:05 +0300

    initial commit

---
+++ calendar.js
@@ -0,0 +1,37 @@
+!(function createList() {
+    const main = document.querySelector('main'),
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    months.forEach(item => createMonthItem(item, main));
+
+    var xmlhttp = new XMLHttpRequest();
+    var url = "schedule.json";
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            myFunction(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    console.log(myArr);
+
+})();
+
+function createMonthItem(item, main) {
+    let section = document.createElement('section'),
+        head = document.createElement('h2'),
+        list = document.createElement('ul');
+
+    head.textContent = item;
+    section.id = item;
+
+    section.append(head);
+    section.append(list);
+
+    main.append(section);
+};
+
+
