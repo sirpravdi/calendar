2766c06e87637695a0c226d3220a028873552597
sirpravdi <ollias17@gmail.com>
2019-02-10 17:53:30 +0300

    test

--- calendar.js
+++ calendar.js
@@ -10,7 +10,6 @@
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
-            myFunction(myArr);
         }
     };
     xmlhttp.open("GET", url, true);
